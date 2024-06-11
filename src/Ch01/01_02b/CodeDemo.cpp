ea6611cc066653e51cabab171b00684ff8a26a70
alexandertestani <alexander.testani@regeneron.com>
2024-06-11 16:55:16 +0000

    added a comment

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,11 @@
 // Learning C++ 
 // Exercise 01_02
-// Hello World, by Eduardo Corpeño 
+// Hello World, by Eduardo Corpeño
+
+#include <iostream>
+
+int main () {
+    std::cout << "Hello world!" << std::endl;
+    std::cout << std::endl << std::endl;
+    return(0);
+}
