84c270b43d24edb097ae79285334c495d5669c31
alexandertestani <alexander.testani@regeneron.com>
2024-06-11 17:01:54 +0000

    added a comment

--- src/Ch01/01_03/CodeDemo.cpp
+++ src/Ch01/01_03/CodeDemo.cpp
@@ -7,8 +7,11 @@
 
 int main(){
     std::string str;
+    std::cout << "Please enter your name: ";
     std::cin >> str;
+    
     std::cout << str;
+    std::cout << " is a great name for a coder!";
 
     std::cout << std::endl << std::endl;
     return (0);
